f0a84c4e65f8c488bbe5ed0314b1e6aac625e0ea
Adarsh Maurya <adarshmaurya0022@gmail.com>
2023-10-15 18:21:09 +0530

    showing timining of news in proper way

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export class Navbar extends Component {
     render() {
         const { fs } = this.props;
         return (
-            <nav style={{fontSize: fs}} className="navbar navbar-expand-lg bg-white py-3 mb-3">
+            <nav style={{ fontSize: fs }} className="navbar navbar-expand-lg bg-white py-3 mb-3">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
@@ -13,25 +13,27 @@ export class Navbar extends Component {
                     <div className="collapse navbar-collapse d-lg-flex justify-content-center" id="navbarNav">
                         <ul className="navbar-nav mt-lg-0 mt-3">
                             <li className="nav-item">
-                                <NavLink className="nav-link  ms-lg-3" aria-current="page" to="/business">business</NavLink>
+                                <NavLink className="nav-link gb gb-bordered hover-slide ms-lg-3" aria-current="page" to="/business">business</NavLink>
+
+
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link ms-lg-3" to="/entertainment">entertainment</NavLink>
+                                <NavLink className="nav-link gb gb-bordered hover-slide ms-lg-3" to="/entertainment">entertainment</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link ms-lg-3" to="/general">general</NavLink>
+                                <NavLink className="nav-link gb gb-bordered hover-slide ms-lg-3" to="/general">general</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link ms-lg-3" to="/health">health</NavLink>
+                                <NavLink className="nav-link gb gb-bordered hover-slide ms-lg-3" to="/health">health</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link ms-lg-3" to="/science">science</NavLink>
+                                <NavLink className="nav-link gb gb-bordered hover-slide ms-lg-3" to="/science">science</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link ms-lg-3" to="/sports">sports</NavLink>
+                                <NavLink className="nav-link gb gb-bordered hover-slide ms-lg-3" to="/sports">sports</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link ms-lg-3" to="/technology">technology</NavLink>
+                                <NavLink className="nav-link gb gb-bordered hover-slide ms-lg-3" to="/technology">technology</NavLink>
                             </li>
                         </ul>
                     </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-    formatPublishedAt = (publishedAt) => {
-        const options = { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' };
-        const date = new Date(publishedAt).toLocaleDateString('en-US', options);
-        return date;
-    }
+    // formatPublishedAt = (publishedAt) => {
+    //     const options = { year: 'numeric', month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+    //     const date = new Date(publishedAt).toLocaleDateString('en-US', options);
+    //     return date;
+    // }
     render() {
         const {title, description, imgurl, publishedAt, author,newsUrl,content} = this.props;
-        const formatPublishedAt = this.formatPublishedAt(publishedAt);
+        // const formatPublishedAt = this.formatPublishedAt(publishedAt);
         return (
             <div className="card border-0 shadow mb-3 overflow-hidden">
                 <div className="row g-0">
@@ -20,10 +20,10 @@ export class NewsItem extends Component {
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
                             <p className="card-text">{content}</p>
-                            <a className='btn btn-sm read' href={newsUrl}>Read more <i className="bi bi-arrow-right-short"></i></a>
+                            <a className='btn btn-sm read' target='_blank' href={newsUrl}>Read more <i className="bi bi-arrow-right-short"></i></a>
                             <div className="card-text d-md-flex mt-2 justify-content-between">
-                                <div><small className="text-body-secondary"><strong>{author}</strong></small></div>
-                                <div><small className="text-body-secondary">Published at {formatPublishedAt}</small></div>
+                                <div><small className="text-body-secondary">Source <strong>{author}</strong></small></div>
+                                <div><small className="text-body-secondary">{publishedAt}</small></div>
                             </div>
                         </div>
                     </div>
